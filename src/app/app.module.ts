59d5a17e6cde750132b71fc175697624706a77a1
volfav <a.volf@enbisys.com>
2018-02-15 15:03:33 +0700

    add map

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AngularOpenlayersModule } from 'ngx-openlayers';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { MockHeroesService } from './mock-heroes.service';
 import { HeroDetailsComponent } from './hero-details/hero-details.component';
+import { MapComponent } from './map/map.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailsComponent
+    HeroDetailsComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AngularOpenlayersModule
   ],
   providers: [MockHeroesService],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,9 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  mapbox: {
+    accessToken: 'pk.eyJ1IjoiYWxleGVpMXZvbGYiLCJhIjoiY2pkanZlOWFlMG0xbTMycTlucml5Yml2eCJ9.0DM-ewCrhp0W1sr8OPJ-ww'
+  }
 };
