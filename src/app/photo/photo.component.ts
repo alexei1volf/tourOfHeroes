43f0b4be513cb5d7aa4a38f421ae9d055a628465
volfav <a.volf@enbisys.com>
2019-04-26 09:53:36 +0700

    another api

--- src/app/photo/photo.component.ts
+++ src/app/photo/photo.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {PhotoService} from "./photo.service";
-import {Observable} from "rxjs";
+import {Observable, Subscription} from "rxjs";
 
 @Component({
   selector: 'photo',
@@ -10,16 +10,26 @@ import {Observable} from "rxjs";
 export class PhotoComponent implements OnInit {
   _photos: any[] = [];
   _count: number = 1;
+  _sub: Subscription;
 
   constructor(private photoService: PhotoService) {}
 
-  ngOnInit(): void {
+/*  ngOnInit(): void {
     const sourceTwo = Observable.fromEvent(document, 'click');
     const exampleTwo = sourceTwo.switchMap(val => this.photoService.getRandomPhotos(this._count));
     exampleTwo.subscribe((response: any[]) => {
       response.forEach(photo => {
-        this._photos.push(photo.urls.full);
+        this._photos.push(photo);
       })
+    });
+  }*/
+
+  ngOnInit(): void {
+    const eventSource = Observable.fromEvent(document, 'click');
+    const internalObservable = eventSource.switchMap(val => this.photoService.getEmployees());
+    this._sub = internalObservable.subscribe((response: any) => {
+      this._photos.slice(this._photos.length, 1);
+      this._photos = response;
     });
   }
 
--- src/app/photo/photo.service.ts
+++ src/app/photo/photo.service.ts
@@ -5,13 +5,19 @@ import {Injectable} from "@angular/core";
 @Injectable()
 export class PhotoService {
 
-  endpoint: string = 'https://api.unsplash.com';
-  accessKey: string = "f70a53720c048e495627a36a95133aeb4990e046d60213d8d1700161e86568b0";
+  /*endpoint: string = 'https://api.unsplash.com';
+  accessKey: string = "f70a53720c048e495627a36a95133aeb4990e046d60213d8d1700161e86568b0";*/
+
+  endpoint: string = 'http://dummy.restapiexample.com';
 
   constructor(private http: HttpClient) {
   }
 
-  getRandomPhotos(count: number): Observable<any> {
+  /*getRandomPhotos(count: number): Observable<any> {
     return this.http.get(`${this.endpoint}/photos/random?client_id=${this.accessKey}&count=${count}`);
+  }*/
+
+  getEmployees(): Observable<any> {
+    return this.http.get(`${this.endpoint}/api/v1/employees`);
   }
 }
