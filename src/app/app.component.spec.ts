15f94114128a3759c909201e57647fbf71769419
volfav <a.volf@enbisys.com>
2019-09-04 09:56:14 +0700

    mock service in provider

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,6 +5,18 @@ import {HeroesService} from "./heroes.service";
 import {Hero} from "./hero";
 
 describe('AppComponent', () => {
+  class HeroesServiceMock {
+    alex: Hero = {
+      id: 1,
+      name: "alex"
+    };
+    expectedHeroes: Hero[] = [this.alex];
+
+    getHeroes() {
+      return this.expectedHeroes;
+    }
+  }
+
   let fixture;
   let app;
 
@@ -15,22 +27,14 @@ describe('AppComponent', () => {
         HeroesComponent
       ],
       providers: [
-         HeroesService,
+        { provide: HeroesService, useClass: HeroesServiceMock },
       ]
     }).compileComponents();
     fixture = TestBed.createComponent(AppComponent);
     app = fixture.debugElement.componentInstance;
   }));
 
-  it('spyOn service getting from injector', () => {
-    let heroesService = fixture.debugElement.injector.get(HeroesService);
-    const alex: Hero = {
-      id: 1,
-      name: "alex"
-    };
-    const expectedHeroes: Hero[] = [alex];
-    spyOn(heroesService, "getHeroes").and.returnValue(expectedHeroes);
-
+  it('should return mocked heroes', () => {
     app.ngOnInit();
 
     expect(app.heroes[0].name).toBe("alex");
