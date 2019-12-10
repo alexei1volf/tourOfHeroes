abb5e27d80e5d9b331493f7349bece60cae16000
volfav <a.volf@enbisys.com>
2019-12-10 11:02:28 +0700

    [form] add validators dynamically

--- src/app/hero-registration-reactive/hero-registration-reactive.component.ts
+++ src/app/hero-registration-reactive/hero-registration-reactive.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup, NgForm} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup, NgForm, Validators} from '@angular/forms';
 import {Hero} from '../hero';
 
 @Component({
@@ -12,17 +12,33 @@ export class HeroRegistrationReactiveComponent implements OnInit {
   public heroForm: FormGroup;
   public hero: Hero;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
-    this.heroForm = new FormGroup({
-      name: new FormControl(),
-      age: new FormControl()
+    this.heroForm = this.fb.group({
+      name: ['alex',
+        [Validators.required, Validators.minLength(3)]],
+      age: 32,
+      email: ['',
+        [Validators.required, Validators.email]
+      ],
+      phone: '',
+      notification: 'email'
     });
   }
 
   onClickSubmit() {
     console.log(this.heroForm);
+  }
+
+  onRadioButtonClick(notification: string) {
+    const phoneControl = this.heroForm.get('phone');
+    if (notification === 'phone') {
+      phoneControl.setValidators(Validators.required);
+    } else {
+      phoneControl.clearValidators();
+    }
+    phoneControl.updateValueAndValidity();
   }
 
 }
